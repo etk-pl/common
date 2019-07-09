e4128ed832e2b817e7295ec4d2f979f1dac031e3
Michał Żaloudik <ponury.kostek@gmail.com>
2019-07-09 12:50:45 +0200

    better `object` tests

--- test/object.js
+++ test/object.js
@@ -5,26 +5,21 @@
 const assert = require("assert");
 const O = require("../src/object");
 describe("object", () => {
+	const input = {
+		a: 1,
+		b: 2,
+		c: 3,
+		e: 4
+	};
+	const keys = Object.keys(input);
 	it("filter", () => {
-		const input = {
-			a: 1,
-			b: 2,
-			c: 3,
-			e: 4
-		};
-		assert.deepStrictEqual(O.filter(input, (value) => value % 2), {
+		assert.deepStrictEqual(O.filter(input, (value, key, object) => keys.includes(key) && object === input && value % 2), {
 			a: 1,
 			c: 3
 		});
 	});
 	it("map", () => {
-		const input = {
-			a: 1,
-			b: 2,
-			c: 3,
-			e: 4
-		};
-		assert.deepStrictEqual(O.map(input, (value) => value * 2), {
+		assert.deepStrictEqual(O.map(input, (value, key, object) => keys.includes(key) && object === input && value * 2), {
 			a: 2,
 			b: 4,
 			c: 6,
@@ -32,26 +27,14 @@ describe("object", () => {
 		});
 	});
 	it("find", () => {
-		const input = {
-			a: 1,
-			b: 2,
-			c: 3,
-			e: 4
-		};
-		assert.deepStrictEqual(O.find(input, (value) => value === 2), [
+		assert.deepStrictEqual(O.find(input, (value, key, object) => value === 2 && key === "b" && object === input), [
 			"b",
 			2
 		]);
 	});
 	it("forEach", () => {
-		const input = {
-			a: 1,
-			b: 2,
-			c: 3,
-			e: 4
-		};
 		const entries = [];
-		O.forEach(input, (value, key) => entries.push([
+		O.forEach(input, (value, key, object) => keys.includes(key) && object === input && entries.push([
 			key,
 			value
 		]));
